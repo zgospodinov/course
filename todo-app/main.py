7a54b74b1eceb974c26715a84dd7f53424839276
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-07 15:38:31 +0300

    Persisting todos in a file

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,9 +1,22 @@
+# filepath: c:\F\code\python-mega-course\course\todo-app\main.py
+def get_todos():
+    try:
+        with open('todos.txt', 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+def save_todos(todos_list):
+    with open('todos.txt', 'w') as file:
+        for todo in todos_list:
+            file.write(todo + '\n')
+
 def show_todos():
     print("\nYour todos:")
     for index, todo in enumerate(todos, 1):
         print(f"{index}. {todo}")
 
-todos = []
+todos = get_todos()
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ").strip().lower()
@@ -12,6 +25,7 @@ while True:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
+            save_todos(todos)
         case "show":
             show_todos()
         case "edit":
@@ -21,6 +35,7 @@ while True:
                 if 1 <= number <= len(todos):
                     new_todo = input("Enter the new todo: ")
                     todos[number - 1] = new_todo
+                    save_todos(todos)
                     print("Todo updated successfully!")
                 else:
                     print("Invalid todo number!")
@@ -32,6 +47,7 @@ while True:
                 number = int(input("Enter the number of the todo to complete: "))
                 if 1 <= number <= len(todos):
                     removed_todo = todos.pop(number - 1)
+                    save_todos(todos)
                     print(f"Todo '{removed_todo}' was completed and removed from the list!")
                 else:
                     print("Invalid todo number!")
