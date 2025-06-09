57121cd561d09e3e898ee78f601716d8b6e93cd7
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-09 16:39:04 +0300

    Todo app: :Edit popup window

--- todo-app/gui_layout.py
+++ todo-app/gui_layout.py
@@ -2,18 +2,29 @@ import FreeSimpleGUI as sg
 import functions
 
 ADD_EVENT = "Add"
+EDIT_EVENT = "Edit"
+SAVE_EVENT = "Save"
 TODOS_KEY = "-TODOS-"
+
+def create_edit_popup(todo_text):
+    layout = [
+        [sg.Text("Edit todo:")],
+        [sg.InputText(todo_text, key="edit_todo")],
+        [sg.Button(SAVE_EVENT), sg.Button("Cancel")]
+    ]
+    return sg.Window("Edit Todo", layout, modal=True)
 
 def create_window():
     label = sg.Text("Type in a to-do")
     input_box = sg.InputText(tooltip="Enter todo", key="todo")
     add_button = sg.Button(ADD_EVENT)
+    edit_button = sg.Button(EDIT_EVENT)
     todos_list = sg.Listbox(values=functions.get_todos(), 
                            key=TODOS_KEY,
                            size=(40, 10))
 
     window = sg.Window('My To-Do App',
                       layout=[[label],
-                             [input_box, add_button],
+                             [input_box, add_button, edit_button],
                              [todos_list]])
     return window
--- todo-app/main-gui.py
+++ todo-app/main-gui.py
@@ -19,5 +19,26 @@ while True:
         functions.save_todos(todos)
         window[layout.TODOS_KEY].update(values=todos)  # Refresh the listbox
         window['todo'].update('')  # Clear the input field
+
+    # Handle Edit button event
+    if event == layout.EDIT_EVENT and len(values[layout.TODOS_KEY]) > 0:
+        todo_to_edit = values[layout.TODOS_KEY][0]  # Get selected todo
+        edit_window = layout.create_edit_popup(todo_to_edit)
+        
+        # Event loop for edit window
+        while True:
+            edit_event, edit_values = edit_window.read()
+            if edit_event in (sg.WIN_CLOSED, "Cancel"):
+                edit_window.close()
+                break
+                
+            if edit_event == layout.SAVE_EVENT:
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = edit_values["edit_todo"]
+                functions.save_todos(todos)
+                window[layout.TODOS_KEY].update(values=todos)  # Refresh main window listbox
+                edit_window.close()
+                break
  
 window.close()
