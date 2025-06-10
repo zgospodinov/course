deaacfdf6ca7c46dfcff3d56d954780ba43e785e
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-10 17:01:54 +0300

    Todo app web ui:  Implement add todo

--- todo-app/main-web.py
+++ todo-app/main-web.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import functions
 
+
+def add_todo():
+    todo = st.session_state.todo_input
+    if todo:
+        try:
+            todos.append(todo)
+            functions.save_todos(todos)
+            st.session_state.todo_input = ""
+        except Exception as e:
+            st.error(f"Error saving todo: {str(e)}")
+    else:
+        st.warning("Please enter a todo before adding.")
+
 st.title("Todo App")
 st.write("Todo app to increase your productivity")
-st.text_input("", placeholder="Add a new todo", key="todo_input")
+st.text_input("", placeholder="Add a new todo", key="todo_input", on_change=add_todo)
 
 try:
     todos = functions.get_todos()
