87db734f3ce01cedee72d6e04ec007e5008b0678
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-11 17:06:28 +0300

    Todo app: Web UI streamlite - complete todo

--- todo-app/main-web.py
+++ todo-app/main-web.py
@@ -6,9 +6,10 @@ def add_todo():
     todo = st.session_state.todo_input
     if todo:
         try:
-            todos.append(todo)
-            functions.save_todos(todos)
+            st.session_state.todos.append(todo)
+            functions.save_todos(st.session_state.todos)
             st.session_state.todo_input = ""
+            st.rerun()
         except Exception as e:
             st.error(f"Error saving todo: {str(e)}")
     else:
@@ -18,14 +19,19 @@ st.title("Todo App")
 st.write("Todo app to increase your productivity")
 st.text_input("", placeholder="Add a new todo", key="todo_input", on_change=add_todo)
 
-try:
-    todos = functions.get_todos()
-except Exception as e:
-    st.error(f"Error loading todos: {str(e)}")
-    todos = []
+if 'todos' not in st.session_state:
+    try:
+        st.session_state.todos = functions.get_todos()
+    except Exception as e:
+        st.error(f"Error loading todos: {str(e)}")
+        st.session_state.todos = []
 
-for todo in todos:
-    st.checkbox(todo, key=todo)
+for index, todo in enumerate(st.session_state.todos):
+    checkbox = st.checkbox(todo, key=f"todo_{index}")
+    if checkbox:
+        st.session_state.todos.pop(index)
+        functions.save_todos(st.session_state.todos)
+        st.rerun()
 
-
-
+# Debug view of session state
+st.session_state
