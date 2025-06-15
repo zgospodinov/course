92bdb7b09cb5b7b641db60c0f15704964d785ffa
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-15 20:04:33 +0300

    Add project descriptions with images and links separated by an empty space with a column

--- portfolio-app/main.py
+++ portfolio-app/main.py
@@ -21,7 +21,7 @@ Feel free to contact me for any questions or collaborations.
 """
 st.write(general_message)
 
-col3, col4 = st.columns(2)
+col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 
@@ -29,9 +29,14 @@ with col3:
     for index, row in df.iterrows():
         if index % 2 == 0:  
             st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"], width=300)
+            st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df.iterrows():
         if index % 2 == 1:  
             st.header(row["title"])
-
+            st.write(row["description"])
+            st.image("images/" + row["image"], width=300)
+            st.write(f"[Source Code]({row['url']})")
