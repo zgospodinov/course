d1c9a38bdd58c7a429446007de020ac4aa9f2c00
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-16 12:42:47 +0300

    Portfolio app: Contact Page integrated with email

--- portfolio-app/pages/Contact_Us.py
+++ portfolio-app/pages/Contact_Us.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="contact_form"):
-    st.text_input("Your Email", key="email")
-    st.text_area("Your Message", key="message")
+    email = st.text_input("Your Email", key="email")
+    message = st.text_area("Your Message", key="message")
     submit_button = st.form_submit_button("Submit")
 
     if submit_button:
+        send_email(email, message)
         st.success("Your message has been sent!")
